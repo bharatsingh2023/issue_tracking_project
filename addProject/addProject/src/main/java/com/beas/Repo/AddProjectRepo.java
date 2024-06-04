ab8dbaab9954fea6a1c5962ee81bd328faf58a73
Bharat Singh <bharatsingh.beasdev@gmail.com>
2024-06-04 14:25:55 +0530

    added motiurs project

---
+++ addProject/addProject/src/main/java/com/beas/Repo/AddProjectRepo.java
@@ -0,0 +1,11 @@
+package com.beas.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beas.entity.AddProjectEntity;
+
+public interface AddProjectRepo extends JpaRepository<AddProjectEntity, Integer>{
+
+	
+
+}
